788bcc58331760079944f9b4a60d44504fd778bc
Nicholas Fisher <fisher@net.chalmers.se>
2019-10-09 12:17:50 +0200

    Connected mainActivity to backend

--- app/src/main/java/se/chalmers/cse/dit341/group00/MainActivity.java
+++ app/src/main/java/se/chalmers/cse/dit341/group00/MainActivity.java
@@ -1,10 +1,9 @@
 package se.chalmers.cse.dit341.group00;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -18,8 +17,7 @@ import com.google.gson.Gson;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import se.chalmers.cse.dit341.group00.model.Camel;
-
+import se.chalmers.cse.dit341.group00.model.Player;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,24 +28,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView myPlayerImage = (ImageView) findViewById(R.id.playerImageView);
+        myPlayerImage.setImageResource(R.drawable.janitor);
+        setPlayerInfo();
+
     }
 
-    public void onClickNewActivity (View view) {
-        TextView mCamelView = findViewById(R.id.camelTextView);
+    public void setPlayerInfo (){
+        final TextView myPlayerName = findViewById(R.id.playerNameTextView);
+        final TextView myPlayerHealth = findViewById(R.id.playerHealthTextView);
+        final TextView myPlayerDamage = findViewById(R.id.playerDamageTextView);
+        final TextView myPlayerDefense = findViewById(R.id.playerDefenseTextView);
+        final TextView myPlayerCurrency = findViewById(R.id.playerCurrencyTextView);
 
-        // Starts a new activity, providing the text from my HTTP text field as an input
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra(HTTP_PARAM, mCamelView.getText().toString());
-        startActivity(intent);
-    }
+        String url = getString(R.string.server_url) + "/api/players";
 
-    public void onClickGetCamels (View view) {
-        // Get the text view in which we will show the result.
-        final TextView mCamelView = findViewById(R.id.camelTextView);
-
-        String url = getString(R.string.server_url) + "/api/camels";
-
-        // This uses Volley (Threading and a request queue is automatically handled in the background)
         RequestQueue queue = Volley.newRequestQueue(this);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -59,32 +54,36 @@ public class MainActivity extends AppCompatActivity {
 
                         String dataArray = null;
 
+
                         try {
-                            dataArray = response.getString("camels");
+                            dataArray = response.getString("players");
+
                         } catch (JSONException e) {
                             Log.e(this.getClass().toString(), e.getMessage());
                         }
 
-                        StringBuilder camelString = new StringBuilder();
-                        camelString.append("This is the list of my camels: \n");
 
-                        Camel[] camels = gson.fromJson(dataArray, Camel[].class);
+                        Player[] players = gson.fromJson(dataArray, Player[].class);
 
-                        for (Camel current : camels) {
-                            camelString.append("Camel is " + current.color + " at "
-                                    + current.position + "\n");
-                        }
+                        String playerDefense = Integer.toString(players[0].defense);
 
-                        mCamelView.setText(camelString.toString());
+                        myPlayerName.setText(players[0].name);
+                        myPlayerDefense.setText(Integer.toString(players[0].defense));
+                        myPlayerHealth.setText(Integer.toString(players[0].health));
+                        myPlayerDamage.setText(Integer.toString(players[0].damage));
+                        myPlayerCurrency.setText(Integer.toString(players[0].currency));
                     }
                 }, new Response.ErrorListener() {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        mCamelView.setText("Error! " + error.toString());
+                        myPlayerName.setText("Error!" + error.toString());
+                        myPlayerHealth.setText("Error!" + error.toString());
+                        myPlayerDamage.setText("Error!" + error.toString());
+                        myPlayerDefense.setText("Error!" + error.toString());
+                        myPlayerCurrency.setText("Error!" + error.toString());
                     }
                 });
-
         // The request queue makes sure that HTTP requests are processed in the right order.
         queue.add(jsonObjectRequest);
     }
