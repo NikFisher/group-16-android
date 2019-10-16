30ed5651c149b1d4145b8e6360b494db69c6d4a5
Simon Johansson <johasim@student.chalmers.se>
2019-10-16 13:34:09 +0200

    added landscape to layouts

--- app/src/main/java/se/chalmers/cse/dit341/group00/MainActivity.java
+++ app/src/main/java/se/chalmers/cse/dit341/group00/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
        /* ImageView myPlayerImage = (ImageView) findViewById(R.id.playerImageView);
         myPlayerImage.setImageResource(R.drawable.janitor);*/
         setPlayerInfo();
