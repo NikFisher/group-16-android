25e01a00c85239d3e6419b4c891c0d9edf2722ae
Tobias Banck <bankt@student.chalmers.se>
2019-10-22 15:14:37 +0200

    change onconfig and getbyID

--- app/src/main/java/se/chalmers/cse/dit341/group00/MainActivity.java
+++ app/src/main/java/se/chalmers/cse/dit341/group00/MainActivity.java
@@ -1,12 +1,16 @@
 package se.chalmers.cse.dit341.group00;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -20,28 +24,51 @@ import com.google.gson.Gson;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import se.chalmers.cse.dit341.group00.model.Boss;
 import se.chalmers.cse.dit341.group00.model.Player;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     // Field for parameter name
     public Player[] players;
-    public Boss [] bosses;
 
 
     public static final String HTTP_PARAM = "httpResponse";
     private View view;
+    private Spinner spinner;
+    private static final String[] paths = {"Simon", "Tobias", "Nick"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position >= 0) {
+                    setPlayerInfo(position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
        /* ImageView myPlayerImage = (ImageView) findViewById(R.id.playerImageView);
         myPlayerImage.setImageResource(R.drawable.janitor);*/
-        setPlayerInfo();
+        getPlayers();
+
+
 
         //button to go to battlefield
         Button bossButton = (Button) findViewById(R.id.boss_btn);
@@ -74,42 +101,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-        public void openBattlefield(){
-            Intent intent = new Intent(this, SecondActivity.class);
-            startActivity(intent);
+    public void openBattlefield(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+    public void openShop(){
+        Intent shopIntent = new Intent(this, ShopActivity.class);
+        startActivity(shopIntent);
+    }
+    public void ressurectPlayer(){
+        TextView ressText = findViewById(R.id.textView9);
+        Button ressButton = (Button) findViewById(R.id.ressurect_btn);
+        if (players[0].health == 100){
+            ressText.setText("You're already at full health");
+            ressButton.setEnabled(false);
         }
-        public void openShop(){
-            Intent shopIntent = new Intent(this, ShopActivity.class);
-            startActivity(shopIntent);
+        else {
+            patchPlayer(100, false);
+            players[0].health = 100;
+            players[0].dead = false;
+            TextView myPlayerHealth = findViewById(R.id.playerHealthTextView);
+            myPlayerHealth.setText(Integer.toString(players[0].health));
+
+            ressButton.setEnabled(false);
+
+            ressText.setText("You have been brought back to life");
         }
-        public void ressurectPlayer(){
-            TextView ressText = findViewById(R.id.textView9);
-            Button ressButton = (Button) findViewById(R.id.ressurect_btn);
-            if (players[0].health == 100){
-                ressText.setText("You're already at full health");
-                ressButton.setEnabled(false);
-            }
-            else {
-                patchPlayer(100, false);
-                players[0].health = 100;
-                players[0].dead = false;
-                TextView myPlayerHealth = findViewById(R.id.playerHealthTextView);
-                myPlayerHealth.setText(Integer.toString(players[0].health));
-
-                ressButton.setEnabled(false);
-
-                ressText.setText("You have been brought back to life");
-            }
-        }
+    }
 
 
 
-    public void setPlayerInfo (){
-        final TextView myPlayerName = findViewById(R.id.playerNameTextView);
-        TextView myPlayerHealth = findViewById(R.id.playerHealthTextView);
-        final TextView myPlayerDamage = findViewById(R.id.playerDamageTextView);
-        final TextView myPlayerDefense = findViewById(R.id.playerDefenseTextView);
-        final TextView myPlayerCurrency = findViewById(R.id.playerCurrencyTextView);
+    public void getPlayers (){
 
         String url = getString(R.string.server_url) + "/api/players";
 
@@ -135,12 +157,66 @@ public class MainActivity extends AppCompatActivity {
 
                         players = gson.fromJson(dataArray, Player[].class);
 
-                        myPlayerName.setText(players[0].name);
-                        myPlayerDefense.setText(Integer.toString(players[0].defense));
-                        myPlayerHealth.setText(Integer.toString(players[0].health));
-                        myPlayerDamage.setText(Integer.toString(players[0].damage));
-                        myPlayerCurrency.setText(Integer.toString(players[0].currency));
-                        setPlayerImage(players[0].damage);
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error);
+
+                    }
+                });
+
+        // The request queue makes sure that HTTP requests are processed in the right order.
+        queue.add(jsonObjectRequest);
+
+    }
+
+    public void setPlayerInfo(int index){
+        TextView myPlayerName = findViewById(R.id.playerNameTextView);
+        TextView myPlayerHealth = findViewById(R.id.playerHealthTextView);
+        TextView myPlayerDamage = findViewById(R.id.playerDamageTextView);
+        TextView myPlayerDefense = findViewById(R.id.playerDefenseTextView);
+        TextView myPlayerCurrency = findViewById(R.id.playerCurrencyTextView);
+
+        String url = getString(R.string.server_url) + "/api/players/" + players[index]._id;
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // GSON allows to parse a JSON string/JSONObject directly into a user-defined class
+                        Gson gson = new Gson();
+
+                        String dataArray = null;
+                        String playerName = null;
+                        String playerHealth = null;
+                        String playerDamage = null;
+                        String playerDefense = null;
+                        String playerCurrency = null;
+
+                        try {
+                            dataArray = null;
+                            playerName = response.getString("name");
+                            playerHealth = response.getString("health");
+                            playerDamage = response.getString("damage");
+                            playerDefense = response.getString("defense");
+                            playerCurrency = response.getString("currency");
+
+                        } catch (JSONException e) {
+                            Log.e(this.getClass().toString(), e.getMessage());
+                        }
+
+                        myPlayerName.setText(playerName);
+                        myPlayerHealth.setText(playerHealth);
+                        myPlayerDamage.setText(playerDamage);
+                        myPlayerDefense.setText(playerDefense);
+                        myPlayerCurrency.setText(playerCurrency);
+
+                        setPlayerImage(players[index].damage);
 
                     }
                 }, new Response.ErrorListener() {
@@ -154,10 +230,7 @@ public class MainActivity extends AppCompatActivity {
                         myPlayerCurrency.setText("Error!" + error.toString());
                     }
                 });
-
-        // The request queue makes sure that HTTP requests are processed in the right order.
         queue.add(jsonObjectRequest);
-
     }
 
     public void setPlayerImage (int damage){
@@ -228,5 +301,65 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(err);
         }
 
+
     }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        setContentView(R.layout.activity_main);
+        getPlayers();
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position >= 0) {
+                    setPlayerInfo(position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        Button bossButton = (Button) findViewById(R.id.boss_btn);
+
+        bossButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openBattlefield();
+            }
+        });
+
+        Button ressButton = (Button) findViewById(R.id.ressurect_btn);
+
+
+        ressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ressurectPlayer();
+
+            }
+        });
+
+        Button shopButton = (Button) findViewById(R.id.shop_button);
+        shopButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openShop();
+            }
+        });
+
+
+
+    }
+
 }
